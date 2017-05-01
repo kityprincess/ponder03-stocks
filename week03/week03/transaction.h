4ae3c65447e3cad751749809f629d3e600a9717b
Matthew Burr <mdburr@outlook.com>
2017-05-01 18:32:32 -0500

    Defined the basic workings of a Transaction class.

--- week03/week03/transaction.h
+++ week03/week03/transaction.h
@@ -1,8 +1,31 @@
-#pragma once
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+
 class Transaction
 {
 public:
-   Transaction();
-   ~Transaction();
+   Transaction(int in_shareCount = 1, float in_sharePrice = 1.0);
+
+   // Gets the count of remaining shares bought in this transaction; 
+   // this can't be added to, but shares can be sold
+   int getShareCount() const { return m_shareCount; }
+
+   // Get share price (share price can't be changed once set at creation time
+   // only read
+   float getSharePrice() const { return m_sharePrice; }
+
+   // Sells a subset of the shares, reducing the share count by the 
+   // amount sold; returns the number of shares sold - in case
+   // a caller tries to sell more shares than available
+   int sellShares(int in_sellCount);
+
+   // Sells all shares from this transaction
+   int sellAllShares();
+
+private:
+   int m_shareCount;
+   float m_sharePrice;
 };
 
+#endif // !TRANSACTION_H
