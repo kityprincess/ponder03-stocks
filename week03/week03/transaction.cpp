1d39779461dabf73064bba75de340b00db93e606
Matthew Burr <mdburr@outlook.com>
2017-05-04 20:01:11 -0500

    Updated portfolio and transaction to conform to remaining tests.

--- week03/week03/transaction.cpp
+++ week03/week03/transaction.cpp
@@ -1,21 +1,27 @@
 #include "transaction.h"
 #include <cassert>
+#include <iostream>
+using namespace std;
 
 /***********************************************************************
 * TRANSACTION :: CONSTRUCTOR
 * "Buys" a number of shares at a given price by creating a new Transaction
 * to represent the purchase
 ************************************************************************/
-Transaction::Transaction(int in_shareCount, Dollars in_sharePrice)
-   : m_shareCount(in_shareCount), m_sharePrice(in_sharePrice)
+Transaction::Transaction(int in_shareCount, Dollars in_sharePrice,
+   Dollars in_soldPrice)
+   : m_shareCount(in_shareCount), m_buyPrice(in_sharePrice),
+   m_soldPrice(in_soldPrice), m_sold(false)
 {
+   if (in_soldPrice > 0)
+      m_sold = true;
 }
 
 /***********************************************************************
 * TRANSACTION :: SELLSHARES
 * Sells a number of shares, reducing the transaction by that amount
 ************************************************************************/
-int Transaction::sellShares(int in_sellCount)
+Transaction Transaction::sellShares(int in_sellCount, Dollars in_salePrice)
 {
    assert(in_sellCount >= 0);
    if (in_sellCount < 0)
@@ -24,23 +30,62 @@ int Transaction::sellShares(int in_sellCount)
    if (in_sellCount > m_shareCount)
       in_sellCount = m_shareCount;
 
+   assert(in_salePrice >= 0);
+   if (in_salePrice < 0)
+      in_salePrice = 0;
+
    m_shareCount -= in_sellCount;
 
+   Transaction salesTransaction(in_sellCount, m_buyPrice, in_salePrice);
+   
    assert(m_shareCount >= 0);
-   assert(in_sellCount >= 0);
-
-   return in_sellCount;
+   assert(m_buyPrice >= 0);
+   assert(m_soldPrice >= 0);
+   
+   return salesTransaction;
 }
 
 /***********************************************************************
-* TRANSACTION :: SELLALLSHARES
-* Sells all the shares contained in this transaction
+* TRANSACTION :: GETPROFIT
+* If this was a sale, gets the profit, else gets 0
 ************************************************************************/
-int Transaction::sellAllShares()
+Dollars Transaction::getProfit() const
 {
-   assert(m_shareCount >= 0);
-   int soldShares = m_shareCount;
-   m_shareCount = 0;
-   
-   return soldShares;
+   if (m_sold)
+      return (m_soldPrice - m_buyPrice) * m_shareCount;
+   else
+      return Dollars(0);
+}
+
+/***********************************************************************
+* TRANSACTION :: DISPLAY
+* Displays the transaction as appropriate depending on whether it is
+* a sale or not
+************************************************************************/
+void Transaction::display(ostream & out) const
+{
+   if (m_sold)
+   {
+      out << "\tSold " << m_shareCount
+         << " shares at " << m_soldPrice
+         << " for a profit of " << getProfit()
+         << endl;
+   }
+   else
+   {
+      out << "\tBought " << m_shareCount
+         << " shares at " << m_buyPrice
+         << endl;
+   }
+}
+
+/***********************************************************************
+* OPERATOR << Transaction
+* Displays the transaction as appropriate depending on whether it is
+* a sale or not
+************************************************************************/
+ostream & operator << (ostream & out, const Transaction & rhs)
+{
+   rhs.display(out);
+   return out;
 }
--- week03/week03/transaction.h
+++ week03/week03/transaction.h
@@ -2,11 +2,13 @@
 #define TRANSACTION_H
 
 #include "dollars.h"
+#include <iostream>
 
 class Transaction
 {
 public:
-   Transaction(int in_shareCount = 1, Dollars in_sharePrice = 1.0);
+   Transaction(int in_shareCount = 1, Dollars in_sharePrice = 1.0,
+      Dollars in_soldPrice = 0.0);
 
    // Gets the count of remaining shares bought in this transaction; 
    // this can't be added to, but shares can be sold
@@ -14,19 +16,30 @@ public:
 
    // Get share price (share price can't be changed once set at creation time
    // only read
-   Dollars getSharePrice() const { return m_sharePrice; }
+   Dollars getSharePrice() const { return m_buyPrice; }
+
+   // Get sold price (sold price can't be changed once set)
+   // only read
+   Dollars getSoldPrice() const { return m_soldPrice; }
+
+   // Gets the profit for this transaction
+   Dollars getProfit() const;
 
    // Sells a subset of the shares, reducing the share count by the 
    // amount sold; returns the number of shares sold - in case
    // a caller tries to sell more shares than available
-   int sellShares(int in_sellCount);
+   Transaction sellShares(int in_sellCount, Dollars in_salePrice);
 
-   // Sells all shares from this transaction
-   int sellAllShares();
+   // Displays the transaction
+   void display(std::ostream & out) const;
 
 private:
    int m_shareCount;
-   Dollars m_sharePrice;
+   bool m_sold;
+   Dollars m_buyPrice;
+   Dollars m_soldPrice;
 };
+
+std::ostream & operator << (std::ostream & out, const Transaction & rhs);
 
 #endif // !TRANSACTION_H
